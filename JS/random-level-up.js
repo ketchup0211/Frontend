84fdbecf5f2dd290cdf33db18f783f3e7af6d57e
cham <dlcka971711@gmail.com>
2023-08-15 18:31:36 +0900

    random-level-up

--- JS/random-level-up.js
+++ JS/random-level-up.js
@@ -24,20 +24,41 @@ const work = () => {
     }
     attempts.innerText = `${parsedAttempt + 1}`;
     console.log('attempts : ' + attempts.innerText);
-    return;
 }
 
 const tryLevelUp = () => {
-    const levelUpLoopId = setInterval(work, 50);
-
-    let currentLevel = document.getElementById('current-level');
-    let parsedLevel = parseInt(currentLevel.innerText);
-    console.log('> current-level : ' + parsedLevel);
-    if (parsedLevel === 10){
-        clearInterval(levelUpLoopId);
+    const levelUpLoopId = setInterval(()=>{
+        work();
+        const currentLevel = document.getElementById('current-level');
+    const parsedLevel = parseInt(currentLevel.innerText);
+    if(parsedLevel === 10){clearInterval(levelUpLoopId);}
     }
-    return;
+        , 50);
+
 };
 
 //work()가 아닌 work 전달
 //level 10이 되면 work 함수 중단하고 싶다. 어떻게?
+
+//answer code
+/*
+const probs = [ 100, 60, 36, 22, 13, 8, 5, 3, 2, 1 ];
+let attempts = 0;
+let level = 0;
+const getRandomBinaryResult = ratio => {
+    const randomNumber = Math.floor(Math.random() * 100);
+    return (randomNumber < ratio);
+};
+const tryLevelUp = () => {
+    const loopId = setInterval(() => {
+        attempts++;
+        const successed = getRandomBinaryResult(probs[level]);
+        if (successed) {
+            level++;
+            document.getElementById('gauge-bar').style.width = level * 10 + '%';
+            document.getElementById('current-level').innerText = level;
+        }
+        document.getElementById('attempts').innerText = attempts;
+        if (level >= 10) clearInterval(loopId);
+}, 50); };
+*/
